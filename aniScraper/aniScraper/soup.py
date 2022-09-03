36109b909878f100b1f4820351a67f266e3a5f73
kienan <leung.kienan@gmail.com>
2022-09-04 03:28:58 +1200

    soup scraps site with target fields

--- aniScraper/aniScraper/soup.py
+++ aniScraper/aniScraper/soup.py
@@ -10,3 +10,11 @@ stuff = doc.find_all(text=re.compile("Jujutsu"), limit=100)
 for ice in stuff:
     print(ice.strip())
 # print(doc)
+
+# need the text field to be based on the input or saved titles.
+
+# user enters in anime name
+# returns list of anime close to entry
+# user picks the anime to add
+# this will save the anime to the db list
+#
