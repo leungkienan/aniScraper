3acc71352a345054528ce4ed4fa357c98fc98abe
kienan <leung.kienan@gmail.com>
2022-09-04 02:38:30 +1200

    soup scraps site with target fields

--- aniScraper/aniScraper/soup.py
+++ aniScraper/aniScraper/soup.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://myanimelist.net/anime/season"
+url = "https://anilist.co/search/anime"
 
 result = requests.get(url)
-# print(result.text)
 doc = BeautifulSoup(result.text, "html.parser")
-stuff = doc.find_all(text="$")
-print(stuff)
+stuff = doc.find_all(text=re.compile("Jujutsu"), limit=100)
+for ice in stuff:
+    print(ice.strip())
+# print(doc)
