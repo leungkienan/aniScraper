0f5a21e94c0a630cf869b54cefeec7d07bb3de95
Kienan Leung <leung.kienan@gmail.com>
2022-10-14 10:54:01 +1300

    working on search function

--- aniScraper/aniScraper/soup.py
+++ aniScraper/aniScraper/soup.py
@@ -2,13 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://anilist.co/search/anime"
+# url = "https://anilist.co/search/anime"
+# url = "https://myanimelist.net/"
+search = "Chainsaw"
+
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-stuff = doc.find_all(text=re.compile("Jujutsu"), limit=100)
-for ice in stuff:
-    print(ice.strip())
+queryS = doc.find_all(text=re.compile(search), limit=100)
+for anime in queryS:
+    print(anime.strip())
+    # if ice is None:
+    #     break
+    # print("no results found")
+
 # print(doc)
 
 # need the text field to be based on the input or saved titles.
--- aniScraper/anime/views.py
+++ aniScraper/anime/views.py
@@ -3,4 +3,3 @@ from django.views.generic import TemplateView
 
 class AnimeView(TemplateView):
     template_name = "base.html"
-
